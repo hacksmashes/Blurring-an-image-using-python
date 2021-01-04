8073a80b43cb7a320d368f4a81e7c36216bd09b0
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 12:16:29 +0530

    Update and rename gaussian.py to gaussian blur.py

--- gaussian blur.py
+++ gaussian blur.py
@@ -1,6 +1,6 @@
 import cv2
 
-image=cv2.imread("johncena.jpg")
+image=cv2.imread("johncena.jpg")                        # Type the file name with extension
 gaussianBlurImg=cv2.GaussianBlur(image,(33,33),0)       # Here (33,33) value respresents the thickness of the blur
 cv2.imshow("Original image",image)
 cv2.imshow("Gaussianblur image",gaussianBlurImg)

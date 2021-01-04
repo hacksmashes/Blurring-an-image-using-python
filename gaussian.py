38bc44a4eef32ff78e19c8adbc871aea7a2b9684
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 12:15:28 +0530

    Add files via upload

---
+++ gaussian.py
@@ -0,0 +1,9 @@
+import cv2
+
+image=cv2.imread("johncena.jpg")
+gaussianBlurImg=cv2.GaussianBlur(image,(33,33),0)       # Here (33,33) value respresents the thickness of the blur
+cv2.imshow("Original image",image)
+cv2.imshow("Gaussianblur image",gaussianBlurImg)
+
+
+# if u want more blur just increse those values ( use only odd numbers )     
